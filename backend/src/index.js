cecd390b64fdd4db55b518b2a0b9bfbda386555e
sudais73 <sudaisman193@gmail.com>
2025-12-12 10:30:18 +0300

    start server function added

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,20 @@ import { connectDB } from './config/db.config.js'
 
 const app = express()
 
-await connectDB()
 app.get('/',(req,res)=>(
     res.send("Nice to meet you sudais☕")
 ))
-app.listen(ENV.PORT, ()=>(
+
+const startServer = async ()=>{
+    try {
+        await connectDB()
+        app.listen(ENV.PORT, ()=>(
     console.log(`server is litening to port:${ENV.PORT}`)
     
 ))
+    } catch (error) {
+        console.log("field to start server", error.message)
+        process.exit(1)
+    }
+}
+startServer();
