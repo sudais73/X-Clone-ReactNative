2cb649cd4b44ecf081cf934040cee9093e7ae82a
sudais73 <sudaisman193@gmail.com>
2025-12-15 11:38:09 +0300

    option added

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,6 +18,17 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8081')
+    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    return res.sendStatus(200)
+  }
+  next()
+})
+
 app.use(express.json())
 
 app.use(clerkMiddleware())
