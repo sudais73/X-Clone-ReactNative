5f39b07a22c90db4b90b0daeb00319ccb5496d34
sudais73 <sudaisman193@gmail.com>
2025-12-16 17:04:33 +0300

    sell checked

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -73,7 +73,7 @@ export const createPost = asyncHandler(async (req, res) => {
     return res.status(400).json({ error: "Post must contain text or image" })
   }
 
-const user = await User.findOne({ clerkId: req.user.id });
+const user = await User.findOne({ clerkId: userId });
   if (!user) {
     return res.status(404).json({ error: "User not found" })
   }
